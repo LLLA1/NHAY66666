5e6e04ebb18ba6ce07908b859bc7417cd4f2a10a
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-08-18 01:26:54 +0300

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -88,7 +88,9 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="⋆ تخطي ⋆", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="⋆ توقف ⋆", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
-            InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)],
+            InlineKeyboardButton(text="السورس", url=config.CHANNEL_LINK),
+            InlineKeyboardButton(text="المتجر", url=f"https://t.me/YMMYN"),
+      ],
     ]
     return buttons
 
@@ -103,7 +105,9 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="⋆ تخطي ⋆", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="⋆ توقف ⋆", callback_data=f"ADMIN Stop|{chat_id}"),
         ],[
-            InlineKeyboardButton(text=config.CHANNEL_NAME, url=config.CHANNEL_LINK)],
+            InlineKeyboardButton(text="السورس", url=config.CHANNEL_LINK),
+            InlineKeyboardButton(text="المتجر", url=f"https://t.me/YMMYN"),
+      ],
     ]
     return buttons
 
