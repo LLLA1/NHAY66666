d977b9fd35c8ee8f5ac83ebf7cd28d28a7e7cd2a
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-09-02 03:55:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/LLLA1/NHAY5",
+    "https://github.com/LLLA1/NHAY66666",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 APK = 5140000000
